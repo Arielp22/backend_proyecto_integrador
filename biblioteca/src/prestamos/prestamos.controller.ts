1201be7c55f74f99ae02881fb02fd4f1c6d6c16b
Arielp22 <earielp2000@hotmail.com>
2026-01-27 10:21:47 -0500

    Correccion usuarios y roles

--- biblioteca/src/prestamos/prestamos.controller.ts
+++ biblioteca/src/prestamos/prestamos.controller.ts
@@ -18,8 +18,7 @@ import { QueryDto } from 'src/common/dto/query.dto';
 export class PrestamosController {
   constructor(private readonly service: PrestamosService) {}
 
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() dto: CreatePrestamoDto) {
     const prestamo = await this.service.create(dto);
@@ -50,7 +49,7 @@ export class PrestamosController {
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
+  @Roles('ADMIN')
   @Put(':id')
   async update(@Param('id') id: string, @Body() dto: UpdatePrestamoDto) {
     const prestamo = await this.service.update(id, dto);
@@ -59,7 +58,7 @@ export class PrestamosController {
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
+  @Roles('ADMIN')
   @Delete(':id')
   async remove(@Param('id') id: string) {
     const prestamo = await this.service.remove(id);
